723dea2cf1f3b1f17a02f7aa34941b5028895daa
Pedro-Eugelmi <Pedrogabrielmendy@gmail.com>
2023-05-11 17:51:46 -0300

    modificando

--- Anotações/script.js
+++
@@ -1,7 +0,0 @@
-let bomba = document.querySelector(".BOMBA")
-let contador = 0
-let trues = true
-while( trues = true){
-    bomba.innerHTML = contador++
-}
-  
--- ShareHope/script.js
+++ ShareHope/script.js
@@ -5,9 +5,51 @@ ScrollReveal({ reset: true ,
     duration:2000});
     
     ScrollReveal().reveal('.main', { origin:'top' });
-    ScrollReveal().reveal('.phrase h1', { origin:'left' });
+    ScrollReveal().reveal('.phrase h1', { origin:'right' });
     ScrollReveal().reveal('#information', { origin:'top' });
+    ScrollReveal().reveal('.inf-area', { origin:'right' });
+    ScrollReveal().reveal('.how-it-works', { origin:'right' });
     ScrollReveal().reveal('#team', { origin:'top' });
-    ScrollReveal().reveal('.portfolio', { origin:'top' });
-    ScrollReveal().reveal('.contact', { origin:'top' });
-    
+/*putting a background on header*/
+
+window.addEventListener('scroll', ()=>
+{
+    let header = document.querySelector('header');
+    let scrollPosition = window.scrollY;
+
+    if(scrollPosition > 10)
+    { header.style.backgroundColor = 'var(--bg-color)'}
+    if(scrollPosition <= 10) {header.style.background = 'none'}
+})
+
+/*Scroll and navs*/
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+window.onscroll = () => 
+{   sections.forEach(sec => { 
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => 
+                { links.classList.remove('selected')
+                  document.querySelector(`header nav a[href*= ${id}]`).classList.add('selected')
+                })
+        }
+    })
+
+}
+
+/*menu */
+let menuIcon = document.querySelector('.header-menu i ');
+let navbar = document.querySelector('.header-menu ul');
+
+menuIcon.addEventListener('click', ()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    if(document.querySelector('header').style.backgroundColor == '' | document.querySelector('header').style.background == 'none'){
+        document.querySelector('header').style.backgroundColor = 'var(--bg-color)'
+    }
+});
